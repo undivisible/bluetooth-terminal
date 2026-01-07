bbfa23d7c1f446587d5abfe19bf8aca620aaf6dc
Max Lee Carter 祁明思 <undivisible@vk.com>
2026-01-07 17:32:12 +1100

    Initial Commit

---
+++ bluetooth terminal/bluetooth_terminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetooth_terminalApp.swift
+//  bluetooth terminal
+//
+//  Created by max carter on 7/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct bluetooth_terminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
